960de9feeeee708984fc772a9d5efaf4407d597c
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-10-29 17:11:10 +0530

    tour-cards commit

---
+++ js_files/scripts.js
@@ -0,0 +1,8 @@
+$('.cards-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev')
+  });
