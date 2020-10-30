45b2feaf5e7f00315addfa8d03a4a3b2b1bc4063
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-10-30 21:54:34 +0530

    change commit

--- js_files/scripts.js
+++ js_files/scripts.js
@@ -10,20 +10,20 @@ $('.cards-slider').slick({
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 850,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 600,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
