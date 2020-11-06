d5db691f8b3358524d4aed6989fd2bccb7d41e2b
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-11-06 23:02:57 +0530

    about section

--- js_files/scripts.js
+++ js_files/scripts.js
@@ -1,69 +1,72 @@
-$(function(){
-  $('.cards-slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    nextArrow: $('.next'),
-    prevArrow: $('.prev'),
-    responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 850,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+$(function() {
+    $('.cards-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
 
 // testimonial section
 
 $('.testimonial-box').owlCarousel({
-  loop:true,
-  margin:8,
-  nav:false,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true,
-  responsive:{
-      0:{
-          items:1
-      }
-  }
-})
+    loop: true,
+    margin: 8,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+});
 
 // reveal text on scroll
 
- function scrollAppear(){
-   var textAppear = document.querySelectorAll('.text-appear');
-   var position = textAppear.getBoundingClientRect().top;
-   var screenPosition = window.innerHeight;
+window.addEventListener('scroll', scrollAppear);
 
-   if(position < screenPosition){
-     textAppear.classList.add('.fade-in');
-   }
- }
-  window.addEventListener('scroll',scrollAppear);
+function scrollAppear() {
+    var textAppear = document.querySelectorAll('.text-appear');
 
- 
+    for (var i = 0; i < textAppear.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = textAppear[i].getBoundingClientRect().top;
+
+        if (revealTop < windowHeight) {
+            textAppear[i].classList.add('fadein');
+        } else {
+            textAppear[i].classList.remove('fadein');
+        }
+    }
+}
