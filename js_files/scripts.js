f22cd1c262669145655b14d412ed35b600c4da67
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-11-10 21:35:51 +0530

    Tour cards corrected

--- js_files/scripts.js
+++ js_files/scripts.js
@@ -1,39 +1,20 @@
-$(function() {
-    $('.cards-slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
-        nextArrow: $('.next'),
-        prevArrow: $('.prev'),
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 850,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+//Tour Cards
+
+$('.cards-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
 });
 
 // testimonial section
