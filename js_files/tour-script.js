d7bad754e98d12260fcb64a80904634639afda8d
radhika20bansal <radhika.bansal_cs18@gla.ac.in>
2020-11-13 16:18:41 +0530

    commit

--- js_files/tour-script.js
+++ js_files/tour-script.js
@@ -1,3 +1,12 @@
+//Navigation Bar
+
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
+
+//Image Slider
+
 $('.carousel').owlCarousel({
     loop: true,
     margin: 0,
